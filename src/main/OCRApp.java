dac000747b181a94b76d1eba9747ebd92f8e50b3
Thomas Schattschneider <ts.schattschneider@gmail.com>
2015-01-11 19:53:53 +0100

    Set system look and feel of the GUI

--- src/main/OCRApp.java
+++ src/main/OCRApp.java
@@ -1,9 +1,31 @@
 package main;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 import gui.Workbench;
 
 public class OCRApp {
 	public static void main(String[] args) {
+		
+		 try {
+	        UIManager.setLookAndFeel(
+	            UIManager.getSystemLookAndFeelClassName());
+	    } 
+	    catch (UnsupportedLookAndFeelException e) {
+	       // TODO handle exception
+	    }
+	    catch (ClassNotFoundException e) {
+	       // TODO handle exception
+	    }
+	    catch (InstantiationException e) {
+	       // TODO handle exception
+	    }
+	    catch (IllegalAccessException e) {
+	       // TODO handle exception
+	    }
+	       
+		
 		new Workbench("OCR Workbench");
 	}
 }
