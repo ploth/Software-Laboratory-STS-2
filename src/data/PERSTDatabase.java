1955ce2ccbd2cd97395a19960436267025edc9de
Pascal <pascal.loth@tuhh.de>
2015-01-11 15:17:28 +0100

    database is working

--- src/data/PERSTDatabase.java
+++ src/data/PERSTDatabase.java
@@ -6,59 +6,75 @@ import org.garret.perst.Persistent;
 import org.garret.perst.Storage;
 import org.garret.perst.StorageFactory;
 
-public class PERSTDatabase{
+public class PERSTDatabase {
 
 	private Storage storage_;
 	private Database db_;
 	private static PERSTDatabase instance_;
 	private static String defaultDatabaseName_ = "perstdatabase.dbs";
-	private PERSTDatabase(String databaseName){
+
+	private PERSTDatabase(String databaseName) {
 		storage_ = StorageFactory.getInstance().createStorage();
-		//TODO: too big?
-		//1GB
-		storage_.open(databaseName, 1024000000); //minimum 40000 bytes (40kbytes)
-		db_ = new Database(storage_, false); 
+		// TODO: too big?
+		// 1GB
+		storage_.open(databaseName, 1024000000); // minimum 40000 bytes
+													// (40kbytes)
+		db_ = new Database(storage_, false);
 		instance_ = this;
 	}
+
 	private Database getDB() {
 		return db_;
 	}
+
 	private Storage getStorage() {
 		return storage_;
 	}
+
 	public class DatabaseElement extends Persistent {
-		private char classification;
-		private char[][] pixels_;
-		private String name = "dick"; //TODO
-		public DatabaseElement(char classification, char[][] pixels){
-			this.classification = classification;
-			this.pixels_ = pixels;
+		private int classification; // int to make query search possible
+		private char[] pixels;
+
+		public DatabaseElement(char classification, char[] pixels) {
+			this.classification = (int) classification;
+			this.pixels = pixels;
 			PERSTDatabase.getInstance().getDB().addRecord(this);
 		}
-		public char getClassification_() {
-			return classification;
+
+		public char getClassification() {
+			return (char) classification;
 		}
-		public char[][] getPixels_() {
-			return pixels_;
+
+		public char[] getPixels() {
+			return pixels;
 		}
 	}
-	public static PERSTDatabase getInstance(){
-		if(instance_ == null){
+
+	public static PERSTDatabase getInstance() {
+		if (instance_ == null) {
 			instance_ = new PERSTDatabase(defaultDatabaseName_);
 		}
 		return instance_;
 	}
-	public void createDatabaseElement(char classification, char[][] pixels){
-		DatabaseElement DatabaseElement = new DatabaseElement(classification, pixels);
+
+	public void createDatabaseElement(char classification, char[] pixels) {
+		DatabaseElement DatabaseElement = new DatabaseElement(classification,
+				pixels);
 	}
-	public void getDatabaseElement(char classification){
-		String string = String.valueOf(classification);
-		for (DatabaseElement e : db_.<DatabaseElement> select(DatabaseElement.class, "name = 'dick'")) {
-			int out = (int)e.getPixels_()[2][2];
-			System.out.println(out);
-		}
-		//IterableIterator<DatabaseElement> iterator = db_.<DatabaseElement> select(DatabaseElement.class, "classification="+classification);
+
+	public IterableIterator<DatabaseElement> getDatabaseIterator() {
+		return db_.<DatabaseElement> getRecords(DatabaseElement.class);
 	}
+
+	public IterableIterator<DatabaseElement> queryDatabaseElements(
+			char classification) {
+		String query = "classification = "
+				+ String.valueOf((int) classification);
+		IterableIterator<DatabaseElement> iterator = db_
+				.<DatabaseElement> select(DatabaseElement.class, query);
+		return iterator;
+	}
+
 	public void closeDB() {
 		db_.commitTransaction();
 		storage_.close();
--- src/test/DatabaseTest.java
+++ src/test/DatabaseTest.java
@@ -1,16 +1,25 @@
 package test;
 
+import org.garret.perst.IterableIterator;
+
 import data.PERSTDatabase;
 import data.PERSTDatabase.DatabaseElement;
 
 public class DatabaseTest {
-	
-	static public void main(String[] args){
+
+	static public void main(String[] args) {
 		PERSTDatabase pdb = PERSTDatabase.getInstance();
-		char classification = 1;
-		char[][] pixels = {{1,2,3},{4,5,6},{7,8,9}};
-		pdb.createDatabaseElement(classification, pixels);
-		pdb.getDatabaseElement(classification);
+		char classification1 = 1;
+		char[] pixels1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+		char classification2 = 2;
+		char[] pixels2 = { 1, 2, 3, 4, 5, 6, 7, 8, 255 };
+		// pdb.createDatabaseElement(classification1, pixels1);
+		// pdb.createDatabaseElement(classification2, pixels2);
+		// pdb.queryDatabaseElements((char) 2);
+		IterableIterator<DatabaseElement> i = pdb.getDatabaseIterator();
+		while (i.hasNext()) {
+			System.out.println((int) i.next().getClassification());
+		}
 		pdb.closeDB();
 	}
 }
