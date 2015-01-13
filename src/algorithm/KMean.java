db647c18906806a0c442854f979c0b0bbb8dfab4
Pascal <pascal.loth@tuhh.de>
2015-01-13 22:07:47 +0100

    made thoughts for kmean. implementation is following

--- src/algorithm/KMean.java
+++ src/algorithm/KMean.java
@@ -1,5 +1,7 @@
 package algorithm;
 
+import data.KdTreeHelper;
+
 public class KMean extends AbstractAlgorithm {
 
 	public KMean() {
@@ -8,8 +10,27 @@ public class KMean extends AbstractAlgorithm {
 
 	@Override
 	public void doSqrEuclid(int k) {
-		// TODO Auto-generated method stub
+		KdTreeHelper treeHelper = KdTreeHelper.getInstance();
+		// select k random prototypes and add to kdtree
 
+		// iterate through every point and check distance to k prototypes in
+		// kdtree and set the cluster value
+
+		// backup the position of all k (vector)
+		// safe them in 2 dimensional array and the index is the cluster value
+
+		// delete kdtree
+
+		// iterate through all points with cluster value 0 for example and
+		// calculate arithmetic mean. the arithmetic mean is to add to a new
+		// kdtree
+
+		// compare arithmetic means to old positions
+		// -> sum up all distances and divide by k (arithmetic mean)
+		// safe to adjustment value
+
+		// start over at "iteration through every point..." if adjustment is
+		// small enough
 	}
 
 	@Override
