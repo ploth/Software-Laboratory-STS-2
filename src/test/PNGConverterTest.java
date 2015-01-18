444d66f33a6485a7718418efc27a581ead802b3d
Pascal <pascal.loth@tuhh.de>
2015-01-18 21:07:11 +0100

    testing png converter

--- src/test/PNGConverterTest.java
+++ src/test/PNGConverterTest.java
@@ -4,36 +4,50 @@ import io.PERST_PNG_Converter;
 
 import java.io.IOException;
 
-import org.garret.perst.IterableIterator;
-
 import data.PERSTDatabase;
 import data.PERSTDatabase.DatabaseElement;
 
 public class PNGConverterTest {
 
 	public static void main(String[] args) throws IOException {
+		// PERSTDatabase db = PERSTDatabase.getInstance();
+		// System.out.println("numberOfDatabaseElements: "
+		// + db.getNumberOfDatabaseElements());
+		// IterableIterator<DatabaseElement> iter = db.getDatabaseIterator();
+		// DatabaseElement e = iter.next();
+		// // which database entry?
+		// int number = 2;
+		// for (int i = 0; i < number; i++) {
+		// e = iter.next();
+		// }
+		// System.out.println("classification: "
+		// + (int) e.getCorrectClassification());
+		// // print array
+		// for (int r = 0; r < Math.sqrt(e.getPixels().length); r++) {
+		// System.out.println();
+		// for (int t = 0; t < Math.sqrt(e.getPixels().length); t++) {
+		// System.out.print((int) e.getPixels()[r
+		// * (int) Math.sqrt(e.getPixels().length) + t]);
+		// }
+		// }
+		// PERST_PNG_Converter.write(e.getCorrectClassification(),
+		// e.getPixels(),
+		// "src/test/test.png");
+		//
+		//
+		// PERST_MNIST_Converter.read("ImageData/train-labels.idx1-ubyte",
+		// "ImageData/train-images.idx3-ubyte", 1, 1, true);
 		PERSTDatabase db = PERSTDatabase.getInstance();
-		System.out.println("numberOfDatabaseElements: "
-				+ db.getNumberOfDatabaseElements());
-		PERST_PNG_Converter png = new PERST_PNG_Converter();
-		IterableIterator<DatabaseElement> iter = db.getDatabaseIterator();
-		DatabaseElement e = iter.next();
-		// which database entry?
-		int number = 2;
-		for (int i = 0; i < number; i++) {
-			e = iter.next();
-		}
-		System.out.println("classification: "
-				+ (int) e.getCorrectClassification());
-		// print array
-		for (int r = 0; r < Math.sqrt(e.getPixels().length); r++) {
-			System.out.println();
-			for (int t = 0; t < Math.sqrt(e.getPixels().length); t++) {
-				System.out.print((int) e.getPixels()[r
-						* (int) Math.sqrt(e.getPixels().length) + t]);
-			}
-		}
-		png.write(e.getCorrectClassification(), e.getPixels(),
-				"src/test/test.png");
+		// System.out.println("numberOfDatabaseElements: "
+		// + db.getNumberOfDatabaseElements());
+		// DatabaseElement e = db.getDatabaseIterator().first();
+		// PERST_PNG_Converter.write(e.getCorrectClassification(),
+		// e.getPixels(),
+		// "bitch.png");
+		PERST_PNG_Converter.read("bitch.png");
+		System.out.println(db.getNumberOfUncorrectDatabaseElements());
+		DatabaseElement e = db.getNonTrainingdataDatabaseIterator().first();
+		// PERST_PNG_Converter.write(classification, pixels, path);
+		// db.closeDB();
 	}
 }
