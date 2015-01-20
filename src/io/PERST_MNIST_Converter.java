fea376fb0dd4aec5bdd7ed9966d587c04abaf5ec
Pascal <pascal.loth@tuhh.de>
2015-01-20 20:02:57 +0100

    mnist converter write function is working.

--- src/io/PERST_MNIST_Converter.java
+++ src/io/PERST_MNIST_Converter.java
@@ -1,9 +1,9 @@
 package io;
 
-import java.io.BufferedWriter;
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.FileInputStream;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 import org.garret.perst.IterableIterator;
@@ -77,18 +77,29 @@ public class PERST_MNIST_Converter extends AbstractConverter {
 			throws IOException {
 		IterableIterator<DatabaseElement> iter = getDb_()
 				.getCorrectDatabaseIterator();
+		DataOutputStream labels = new DataOutputStream(new FileOutputStream(
+				writeLabelPath));
+		DataOutputStream images = new DataOutputStream(new FileOutputStream(
+				writeImagePath));
 		int dim = getDb_().getDim();
-		FileWriter fwLabel = new FileWriter(writeLabelPath);
-		FileWriter fwImage = new FileWriter(writeImagePath);
-		BufferedWriter bwLabel = new BufferedWriter(fwLabel);
-		BufferedWriter bwImage = new BufferedWriter(fwImage);
+		// magic numers
+		images.writeInt(2051); // TODO
+		labels.writeInt(2049);
+		// number of items
+		images.writeInt(getDb_().getNumberOfCorrectDatabaseElements());
+		labels.writeInt(getDb_().getNumberOfCorrectDatabaseElements());
+		// image dimensions (image file)
+		images.writeInt(dim);
+		images.writeInt(dim);
 		while (iter.hasNext()) {
+			// labels
 			DatabaseElement e = iter.next();
-			bwImage.write(e.getPixels());
+			labels.writeByte((int) e.getCorrectClassification());
+			// pixels
+			for (int i = 0; i < (dim * dim); i++) {
+				images.writeByte((int) e.getPixels()[i]);
+			}
 		}
-		bwLabel.close();
-		bwImage.close();
-		fwLabel.close();
-		fwImage.close();
+		labels.close();
 	}
 }
