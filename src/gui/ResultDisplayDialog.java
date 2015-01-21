1288e8e17526353e7353fcdeec0db5f75e91a612
Thomas Schattschneider <ts.schattschneider@gmail.com>
2015-01-21 23:46:26 +0100

    Fixed ocurrence of null pointer string

--- src/gui/ResultDisplayDialog.java
+++ src/gui/ResultDisplayDialog.java
@@ -190,7 +190,8 @@ public class ResultDisplayDialog extends JDialog implements ActionListener {
 	private void enterCorrectClassification() {
 		String enteredClassification_str = JOptionPane
 				.showInputDialog("Enter correct classification");
-		if (enteredClassification_str.isEmpty()) { // TODO exception
+		if (enteredClassification_str == null
+				|| enteredClassification_str.isEmpty()) {
 			JOptionPane.showMessageDialog(new JFrame(),
 					"Please enter a number between 0 and 9.");
 			currentlyEnteredClassification = PERSTDatabase.NO_CORRECT_CLASSIFICATION;
