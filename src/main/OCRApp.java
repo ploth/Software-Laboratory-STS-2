a87fbec7120e143aeef60e6120a473d97e7ea203
Thomas Schattschneider <ts.schattschneider@gmail.com>
2015-01-21 14:47:45 +0100

    Added exception handling.

--- src/main/OCRApp.java
+++ src/main/OCRApp.java
@@ -2,6 +2,8 @@ package main;
 
 import gui.Workbench;
 
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
@@ -10,13 +12,36 @@ public class OCRApp {
 		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		} catch (UnsupportedLookAndFeelException e) {
-			// TODO handle exception
+			JOptionPane
+					.showMessageDialog(
+							new JFrame(),
+							"System look and feel is not supported!\n"
+									+ e.getMessage(),
+							"Unsupported look and feel",
+							JOptionPane.ERROR_MESSAGE);
+			System.exit(1);
 		} catch (ClassNotFoundException e) {
-			// TODO handle exception
+			JOptionPane.showMessageDialog(
+					new JFrame(),
+					"The LookAndFeel class could not be found.\n"
+							+ e.getMessage(), "LookAndFeel class not found",
+					JOptionPane.ERROR_MESSAGE);
+			System.exit(1);
 		} catch (InstantiationException e) {
-			// TODO handle exception
+			JOptionPane.showMessageDialog(new JFrame(),
+					"Could not create a new instance of the LookAndFeel class.\n"
+							+ e.getMessage(), "InstantiationException",
+					JOptionPane.ERROR_MESSAGE);
+			System.exit(1);
 		} catch (IllegalAccessException e) {
-			// TODO handle exception
+			JOptionPane
+					.showMessageDialog(
+							new JFrame(),
+							"An illegal access exception ocurred.\n"
+									+ "This probably means, that access to some libraries is restricted.\n"
+									+ e.getMessage(), "Illegal access",
+							JOptionPane.ERROR_MESSAGE);
+			System.exit(1);
 		}
 		new Workbench("OCR Workbench");
 	}
